bdf139fcb5151579134f2c532c72f4943cc18302
funwarioisii <kazuyukihashimoto2006@gmail.com>
2024-01-06 18:13:37 +0900

    remove spec

--- spec/rails_erd_mermaid_spec.rb
+++ spec/rails_erd_mermaid_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RailsErdMermaid do
   it "has a version number" do
     expect(RailsErdMermaid::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
